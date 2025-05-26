5032e550b271e8770689533de4c88233b65e8bb1
jparkk0517 <redysj0517@naver.com>
2025-05-26 21:37:25 +0900

    feat: 채팅 본문 컴포넌트 최적화 및 첫 질문 처리 로직 개선

--- frontend/src/feature/chat/ui/ChatBody.tsx
+++ frontend/src/feature/chat/ui/ChatBody.tsx
@@ -2,44 +2,42 @@ import Message from '../../../entity/chat/ui/Message';
 import { useRef, useEffect } from 'react';
 import { useMemo } from 'react';
 import { useChatHistory, useRequest } from '../hook/useChat';
-import { useCallback } from 'react';
+import { memo } from 'react';
 
 const ChatBody = () => {
+  const chatBodyRef = useRef<HTMLDivElement | null>(null);
   const isFirst = useRef(true);
   const { nextQuestion } = useRequest();
-  const { data, isFetching, refetch } = useChatHistory();
+  const { data, refetch } = useChatHistory();
   const chatEndRef = useRef<HTMLDivElement | null>(null);
 
   const messages = useMemo(() => {
     return data?.map((message) => <Message {...message} key={message.id} />);
   }, [data]);
 
-  const firstQuestionDebounce = useCallback(() => {
-    if (!isFirst.current) return;
-    isFirst.current = false;
-    nextQuestion().then(() => {
-      refetch();
-    });
-  }, [nextQuestion, refetch]);
   useEffect(() => {
-    if (data?.length === 0 && !isFetching) {
-      return firstQuestionDebounce();
+    if (data?.length === 0) {
+      if (!isFirst.current) return;
+      isFirst.current = false;
+      nextQuestion().then(() => {
+        refetch();
+      });
     }
 
     if (chatEndRef.current) {
-      setTimeout(() => {
-        chatEndRef.current?.scrollIntoView();
-      }, 0);
+      chatEndRef.current?.scrollIntoView();
     }
-  }, [data, firstQuestionDebounce, isFetching]);
+  }, [data, nextQuestion, refetch]);
+
   return (
     <div
       className='flex flex-col border-b-2 border-gray-300 overflow-scroll flex-grow'
-      style={{ height: 'calc(95% - 100px)' }}>
+      style={{ height: 'calc(95% - 100px)' }}
+      ref={chatBodyRef}>
       {messages}
       <div ref={chatEndRef} />
     </div>
   );
 };
 
-export default ChatBody;
+export default memo(ChatBody);
