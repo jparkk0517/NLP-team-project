b6bdf8d0daea79f184d7669ec2e36a0bc3adb2ab
jparkk0517 <redysj0517@naver.com>
2025-05-26 20:45:09 +0900

    feat: 채팅 입력 기능 개선 및 상태 관리 최적화

--- frontend/src/feature/chat/ui/ChatInput.tsx
+++ frontend/src/feature/chat/ui/ChatInput.tsx
@@ -1,51 +1,45 @@
 import { useCallback } from 'react';
 import Button from '../../../shared/Button';
 import useFrontStore from '../../../shared/frontStore';
-import { useActionState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useChat } from '../hook/useChat';
 import TextArea from '../../../shared/TextArea';
-
-type State = {
-  message: string;
-};
-const initialState: State = {
-  message: '',
-};
+import { useState } from 'react';
 
 const ChatInput = () => {
-  const { mutateAsync: chat } = useChat();
   const queryClient = useQueryClient();
-  const chatAction = useCallback(
-    async (initialState: State, formData: FormData) => {
+  const [message, setMessage] = useState('');
+  const { mutateAsync: chat, isPending } = useChat();
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
       try {
-        const message = (formData.get('message') as string) ?? '';
         await chat(message);
         queryClient.invalidateQueries({ queryKey: ['chatHistory'] });
       } catch (error) {
         console.error(error);
       }
-      return initialState;
+      setMessage('');
     },
-    [chat, queryClient]
+    [chat, message, queryClient]
   );
-  const [state, action, isPending] = useActionState(chatAction, initialState);
   const { inputable } = useFrontStore();
-  const isDisabled = !inputable || isPending;
   return (
-    <form className='h-[10%] p-4 flex flex-row' action={action}>
+    <form className='h-[10%] p-4 flex flex-row' onSubmit={handleSubmit}>
       <TextArea
-        placeholder={isDisabled ? 'Action을 선택해주세요' : '메시지 입력'}
-        defaultValue={state.message}
+        placeholder={inputable ? 'Action을 선택해주세요' : '메시지 입력'}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         name='message'
         className='w-[90%] h-full border-2 border-gray-300 rounded-md p-2'
-        disabled={isDisabled}
+        disabled={!inputable}
       />
       <Button
         type='submit'
         className={`w-[10%] min-w-[70px]`}
         isLoading={isPending}
-        disabled={isDisabled}>
+        disabled={!inputable || message.length === 0}>
         전송
       </Button>
     </form>
